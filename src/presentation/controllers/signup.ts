35052123830e6ca03594fde312561b4881f224fe
Gabriel Borba Marques <xeroxsulensino@hotmail.com>
2022-03-05 19:05:45 -0300

    feat: tests email valid

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,8 +2,15 @@ import { httpRequest, httpResponse } from "../protocols/http";
 import { badRequest } from "../helpers/http-helpers";
 import { MissignParamError } from "../errors/missing-param-error";
 import { Controler } from "../protocols/controller";
+import { EmailValidator } from "../protocols/email-validator";
+import { InvalidParamError } from "../errors/invalid-param-error ";
 
 export class SignUpController implements Controler {
+  private readonly emailValidator: EmailValidator;
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle(httpRequest: httpRequest): httpResponse {
     const requiredFields = [
       "name",
@@ -15,6 +22,10 @@ export class SignUpController implements Controler {
       if (!httpRequest.body[field]) {
         return badRequest(new MissignParamError(field));
       }
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email);
+    if (!isValid) {
+      return badRequest(new InvalidParamError("email"));
     }
   }
 }
