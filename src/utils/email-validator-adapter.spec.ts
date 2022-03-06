1261abf79c771cd398ae0540128fd7e26e0c1a85
Gabriel Borba Marques <xeroxsulensino@hotmail.com>
2022-03-06 11:38:53 -0300

    feat: email-validator

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -3,7 +3,12 @@ import { EmailValidatorAdapter } from './email-validator'
 describe('EmailValidator Adapter', () => {
   it('should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@email.com')
+    const isValid = sut.isValid('invalid_emailemail.com')
     expect(isValid).toBe(false)
+  })
+  it('should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@email.com')
+    expect(isValid).toBe(true)
   })
 })
